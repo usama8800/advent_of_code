7c2c013121ed2ab088ca48d6e4f654361a99a8e0
Usama Ahsan <usama8800@gmail.com>
2022-09-22 23:30:05 +0500

    Day12

--- src/day12.rs
+++ src/day12.rs
@@ -1,10 +1,12 @@
 use color_eyre::eyre::{eyre, Result};
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 #[derive(Debug)]
 struct Cave {
     is_big: bool,
-    visited: bool,
     connections: Vec<String>,
 }
 
@@ -18,19 +20,19 @@ fn get_input() -> Result<HashMap<String, Cave>> {
         }
     });
 
-    let mut ret = HashMap::new();
+    let mut ret: HashMap<String, Cave> = HashMap::new();
     for line in lines {
         let splits: Vec<&str> = line.split('-').collect();
-        for i in 0..2 {
-            if let Some(curr) = ret.get(splits[1 - i]) {
-                curr.
+        for curr in 0..2 {
+            let other: usize = if curr == 0 { 1 } else { 0 };
+            if let Some(existing) = ret.get_mut(splits[curr]) {
+                existing.connections.push(splits[other].to_owned());
             } else {
                 ret.insert(
-                    splits[1 - 0].to_owned(),
+                    splits[curr].to_owned(),
                     Cave {
-                        connections: Vec::from([splits[1 - i].to_owned()]),
-                        is_big: splits[1 - i].chars().next().unwrap().is_uppercase(),
-                        visited: false,
+                        connections: Vec::from([splits[other].to_owned()]),
+                        is_big: splits[curr].chars().next().unwrap().is_uppercase(),
                     },
                 );
             }
@@ -41,14 +43,90 @@ fn get_input() -> Result<HashMap<String, Cave>> {
 }
 
 fn solve_p1() -> Result<()> {
-    let input = get_input()?;
-    dbg!(input);
+    let mut caves = get_input()?;
+
+    let mut stack: Vec<(String, Vec<String>)> = Vec::from([("start".to_owned(), Vec::new())]);
+    let mut ends = 0;
+    loop {
+        if let Some((mut visiting_str, mut visited)) = stack.pop() {
+            // dbg!(&visiting_str);
+            let visiting = caves.get(&visiting_str).unwrap();
+            visited.push(visiting_str.clone());
+
+            if visiting_str.as_str() == "end" {
+                // dbg!(&visited);
+                ends += 1;
+                continue;
+            }
+
+            for connection_str in visiting.connections.iter() {
+                let connection = caves.get(connection_str).unwrap();
+                if visited.contains(connection_str) && !connection.is_big {
+                    continue;
+                }
+                stack.push((connection_str.clone(), visited.clone()));
+            }
+        } else {
+            break;
+        }
+    }
+    dbg!(ends);
 
     Ok(())
 }
 
 pub fn solve_p2() -> Result<()> {
-    let input = get_input()?;
+    let mut caves = get_input()?;
+
+    let mut stack: Vec<(String, Option<String>, Vec<String>)> =
+        Vec::from([("start".to_owned(), None, Vec::new())]);
+    let mut ends = 0;
+    loop {
+        if let Some((mut visiting_str, doubled_option, mut visited)) = stack.pop() {
+            // dbg!(&visiting_str);
+            let visiting = caves.get(&visiting_str).unwrap();
+
+            let doubled = if visited.contains(&visiting_str) && !visiting.is_big {
+                Some(&visiting_str)
+            } else {
+                if let Some(ref old) = doubled_option {
+                    Some(old)
+                } else {
+                    None
+                }
+            };
+            visited.push(visiting_str.clone());
+
+            if visiting_str.as_str() == "end" {
+                // dbg!(&visited);
+                ends += 1;
+                continue;
+            }
+
+            for connection_str in visiting.connections.iter() {
+                if connection_str == "start" {
+                    continue;
+                }
+                // dbg!(connection_str);
+                let connection = caves.get(connection_str).unwrap();
+                if let Some(doubled_str) = doubled {
+                    if visited.contains(connection_str) && !connection.is_big {
+                        continue;
+                    }
+                    stack.push((
+                        connection_str.clone(),
+                        Some(doubled_str.to_owned()),
+                        visited.clone(),
+                    ));
+                } else {
+                    stack.push((connection_str.clone(), None, visited.clone()));
+                }
+            }
+        } else {
+            break;
+        }
+    }
+    dbg!(ends);
 
     Ok(())
 }
